da09120592ce91adb16cc6da4d844e92850a90fc
Elaine Alasagas <ealasagas@gmail.com>
2025-05-26 06:00:52 -0700

    adding fonts

---
+++ src/components/Logo.tsx
@@ -0,0 +1,18 @@
+import '../assets/fonts/fonts.css'
+
+function Logo() {
+    return (
+        <div style={{
+            color: 'white', 
+            lineHeight: 0.9, 
+            fontSize: '6rem',
+            textAlign: 'center',
+            zIndex: '100'
+            }}>
+            <div style={{fontFamily: 'LeJourScript', textShadow: '4px 4px 2px #b26977'}}>yummi</div>
+            <div style={{fontFamily: 'LeJourSerif', textShadow: '4px 4px 2px #d7a6a2'}}>HAIR</div>
+        </div>
+    )
+}
+
+export default Logo;
---
+++ src/home.tsx
@@ -0,0 +1,58 @@
+import Logo from "./components/Logo";
+import './assets/fonts/fonts.css';
+import './home.css'; 
+import flower from './assets/graphics/flower.svg';
+import rose_branch from './assets/graphics/branch_rosecolor.svg';
+
+function Home() {
+  return (
+    <div>
+        <img
+        src={rose_branch}
+        alt="branch outline"
+        style={{ 
+            marginTop: '6rem', 
+            maxWidth: '17rem',
+            position: 'absolute',
+            right: '-113px',
+            zIndex: '0',
+            rotate: '45deg'
+        }}
+      />
+      <img
+        src={rose_branch}
+        alt="branch outline"
+        style={{ 
+            maxWidth: '14rem',
+            position: 'absolute',
+            left: '-75px',
+            bottom: '-1px',
+            zIndex: '0',
+            rotate: '119deg'
+        }}
+      />
+      <div className='center-screen'>
+        <img
+        src={flower}
+        alt="Flower outline"
+        style={{ 
+            marginTop: '1rem', 
+            maxWidth: '18rem',
+            position: 'absolute',
+            top: '20%',
+            zIndex: '0'
+        }}
+      />
+        <Logo />
+        <div className='julius-sans-one' style={{
+            color: 'white',
+            textAlign: 'center',
+            fontSize: '1.9rem',
+            margin: '10px 10% 0 10%',
+        }}>Dream Hair <p style={{margin: '0'}}>For</p> Everyone</div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
