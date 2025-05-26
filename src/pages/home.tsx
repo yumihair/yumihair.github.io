8e9c6f2dddd3c2a1356b6da57ebc9f6cf253b9b0
Elaine Alasagas <ealasagas@gmail.com>
2025-05-26 08:58:10 -0700

    cropping branch home image

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,57 +4,60 @@ import '../assets/fonts/fonts.css'
 import './home.css'; 
 import flower from '../assets/graphics/flower.svg';
 import rose_branch from '../assets/graphics/branch_rosecolor.svg';
-
 function Home() {
   return (
-    <div>
-        <img
+    <div style={{ position: 'relative', overflow: 'hidden', height: '100vh' }}>
+      <img
         src={rose_branch}
         alt="branch outline"
-        style={{ 
-            marginTop: '6rem', 
-            maxWidth: '17rem',
-            position: 'absolute',
-            right: '-113px',
-            zIndex: '0',
-            rotate: '45deg',
-            overflow: 'hidden'
-            
+        style={{
+          marginTop: '6rem',
+          maxWidth: '17rem',
+          position: 'absolute',
+          right: '-113px',
+          zIndex: '0',
+          rotate: '45deg',
+          overflow: 'hidden',
         }}
       />
       <img
         src={rose_branch}
         alt="branch outline"
-        style={{ 
-            maxWidth: '14rem',
-            position: 'absolute',
-            left: '-75px',
-            bottom: '-1px',
-            zIndex: '0',
-            rotate: '119deg',
-            overflow: 'hidden'
+        style={{
+          maxWidth: '14rem',
+          position: 'absolute',
+          left: '-75px',
+          bottom: '-1px',
+          zIndex: '0',
+          rotate: '119deg',
+          overflow: 'hidden',
         }}
       />
-      <div className='center-screen'>
+      <div className="center-screen">
         <img
-        src={flower}
-        alt="Flower outline"
-        style={{ 
-            marginTop: '1rem', 
+          src={flower}
+          alt="Flower outline"
+          style={{
+            marginTop: '1rem',
             maxWidth: '18rem',
             position: 'absolute',
             top: '24%',
-            zIndex: '0'
-        }}
-      />
+            zIndex: '0',
+          }}
+        />
         <Logo />
-        <div className='julius-sans-one' style={{
+        <div
+          className="julius-sans-one"
+          style={{
             color: 'white',
             textAlign: 'center',
             fontSize: '1.9rem',
             margin: '10px 10% 10% 10%',
-            lineHeight: '1'
-        }}>Dream Hair <p style={{margin: '0'}}>For</p> Everyone</div>
+            lineHeight: '1',
+          }}
+        >
+          Dream Hair <p style={{ margin: '0' }}>For</p> Everyone
+        </div>
         <BookButton name="Book an appointment"link="https://yummi-hair.square.site/" />
       </div>
     </div>
