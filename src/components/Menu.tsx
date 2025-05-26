65018971e75b811f3c1b04090b6c93320ebeb636
Elaine Alasagas <ealasagas@gmail.com>
2025-05-26 06:57:32 -0700

    adding react-icons

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Navbar, Nav } from 'react-bootstrap';
+import { FaFacebookF, FaInstagram, FaXTwitter } from 'react-icons/fa6'; // Icons
 
 function Menu() {
   return (
@@ -8,10 +9,26 @@ function Menu() {
         <Navbar.Brand href="#home">My App</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
+          <Nav className="me-auto w-100">
+            <Nav.Link href="#book">Book</Nav.Link>
+            <Nav.Link href="#services">Services</Nav.Link>
+            <Nav.Link href="#shop">Shop</Nav.Link>
+            <Nav.Link href="#gallery">Gallery</Nav.Link>
             <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="#faq">FAQ</Nav.Link>
+
+            {/* Social media line */}
+            <div className="d-flex gap-3 mt-2 mt-lg-0 ms-lg-auto social-links">
+              <Nav.Link href="https://facebook.com" target="_blank">
+                <FaFacebookF />
+              </Nav.Link>
+              <Nav.Link href="https://x.com" target="_blank">
+                <FaXTwitter />
+              </Nav.Link>
+              <Nav.Link href="https://instagram.com" target="_blank">
+                <FaInstagram />
+              </Nav.Link>
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Container>
