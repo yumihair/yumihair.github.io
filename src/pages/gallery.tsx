6b7c83209748bde51b2d75257e9db78006164b69
Elaine Alasagas <ealasagas@gmail.com>
2025-05-29 04:29:49 -0700

    checking api key if issue

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -10,40 +10,40 @@ interface GalleryProps {
 const Gallery: React.FC<GalleryProps> = ({ folderId, apiKey }) => {
   const [imageUrls, setImageUrls] = useState<string[]>([]);
 
-  useEffect(() => {
+    useEffect(() => {
     const fetchImages = async () => {
-      try {
-        const url = `https://www.googleapis.com/drive/v3/files?q='${folderId}'+in+parents&key=${apiKey}&fields=files(id,name,mimeType)`;
+        try {
+        const url = `https://www.googleapis.com/drive/v3/files?q='${folderId}'+in+parents and mimeType contains 'image/'&key=${apiKey}&fields=files(id,name,webContentLink)`;
         const response = await fetch(url);
         const data = await response.json();
 
         if (data.files) {
-          const imageLinks = data.files
-            .filter((file: any) => file.mimeType.startsWith('image/'))
-            .map((file: any) => `https://drive.google.com/uc?export=view&id=${file.id}`);
-          setImageUrls(imageLinks);
+            const imageLinks = data.files
+            .slice(0, 4)
+            .map((file: any) => file.webContentLink.replace("&export=download", "&export=view"));
+            setImageUrls(imageLinks);
         }
-      } catch (error) {
+        } catch (error) {
         console.error('Error fetching Google Drive images:', error);
-      }
+        }
     };
 
     fetchImages();
-  }, [folderId, apiKey]);
+}, [folderId, apiKey]);
 
-    return(
-        <div style={{ minHeight: '100vh', position: 'relative' }}>
-              <Title name="Gallery" cursiveName="hair" />
+  return (
+    <div style={{ minHeight: '100vh', position: 'relative' }}>
+      <Title name="Gallery" cursiveName="hair" />
 
-              <div className="grid-container">
-                {imageUrls.map((url, idx) => (
-                    <div key={idx} className="grid-item">
-                    <img src={url} alt={`Drive Image ${idx + 1}`} />
-                    </div>
-                ))}
-                </div>
-        </div>
-    )
+      <div className="grid-container">
+        {imageUrls.map((url, idx) => (
+          <div key={idx} className="grid-item">
+            <img src={url} alt={`Drive Image ${idx + 1}`} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Gallery;
